5a7d87c7af1c62b1ff091792e3ca4eadf1b59e05
Navid Haghighi <navidhaghighi@Navids-MBP-2.fios-router.home>
2020-04-10 11:02:01 -0400

    NH - added new files for project using rekognition

---
+++ 03-videolyzer/label-detection.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+import boto
+import boto3
+session=boto3.Session()
+get_ipython().run_line_magic('clear', '')
+s3=session.resource('s3')
+bucket=s3.create_bucket('videolyzer-navid')
+bucket=s3.create_bucket(Bucket='videolyzer-navid')
+bucket
+get_ipython().run_line_magic('ls', '/Users/navidhaghighi/Downloads/*.mp4')
+pathname='/Users/navidhaghighi/Downloads/Pexels Videos 1466210.mp4'
+from pathlib import Path
+path=Path(pathname).expanduser().resolve()
+print(path)
+path.name
+bucket.upload_file(str(path), str(path.name))
+rekognition_client=session.client('rekognition')
+response = client.start_label_detection(Video={'S3Object': {'Bucket': bucket.name,'Name': path.name}})
+response = rekognition_client.start_label_detection(Video={'S3Object': {'Bucket': bucket.name,'Name': path.name}})
+response
+job_id=response['JobId']
+result=response = client.get_label_detection(
+    JobId=job_id
+)
+result=rekognition_client.get_label_detection(
+    JobId=job_id
+)
+result
+result.keys()
+result['Labels']
+len(result['Labels'])
+get_ipython().run_line_magic('save', 'label-detection.py 1-30')
